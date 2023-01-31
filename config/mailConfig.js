acf5be5b492fc90e8953c9c78c9ee16dd763be48
Sourabh <sourabhp94503@gmail.com>
2023-01-31 16:53:49 +0530

    Files required for offline added and debugged

--- config/mailConfig.js
+++ config/mailConfig.js
@@ -1,16 +1,19 @@
 const nodemailer = require("nodemailer")
 
-const auth = {
-  type: "OAuth2",
-  user: "agropointofficial@gmail.com",
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  refreshToken: process.env.REFRESH_TOKEN,
-}
+// const auth = {
+//   type: "OAuth2",
+//   user: "agropointofficial@gmail.com",
+//   clientId: process.env.CLIENT_ID,
+//   clientSecret: process.env.CLIENT_SECRET,
+//   refreshToken: process.env.REFRESH_TOKEN,
+// }
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
-  auth: auth
+  auth: {
+    user: process.env.email,
+    pass: process.env.password
+  }
 })
 
 module.exports = {
